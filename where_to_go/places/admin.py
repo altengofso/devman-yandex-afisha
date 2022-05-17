f748398645df0e38e30ffee17b8af2ec00a1ccca
altengof_so <altengofso@gmail.com>
2022-05-18 01:33:50 +0300

    Admin site: image previews

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Place, Image
-
-
-admin.site.register(Image)
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ['preview']
+    fields = ['image', 'preview', 'order']
+
+    def preview(self, image):
+        return format_html('<img src="{}" height="200"/>', image.image.url)
 
 
 @admin.register(Place)
@@ -14,3 +18,11 @@ class PlaceAdmin(admin.ModelAdmin):
     inlines = [
         ImageInline,
     ]
+
+
+# @admin.register(Image)
+# class ImageAdmin(admin.ModelAdmin):
+#     readonly_fields = ['preview']
+#
+#     def preview(self, image):
+#         return format_html('<img src="{}" height="200"/>', image.image.url)
