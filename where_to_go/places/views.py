8a71294f2fd936bb7491a1f0cfc5d5ff076f2ca5
altengof_so <altengofso@gmail.com>
2022-05-17 23:30:42 +0300

    Move data from template to view

--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Place
+
+
+def index(request):
+    data = {
+        'type': 'FeatureCollection',
+        'features': [],
+    }
+
+    for place in Place.objects.all():
+        # feature = {
+        #     'type': 'Feature',
+        #     'geometry': {
+        #         'type': 'Point',
+        #         'coordinates': [place.longitude, place.latitude],
+        #     },
+        #     'properties': {
+        #         'title': place.title,
+        #         'placeId': place.id,
+        #         'detailsUrl': {
+        #             'title': place.title,
+        #             'imgs': [image.image.url for image in place.image_set.all()],
+        #             'description_short': place.description_short,
+        #             'description_long': place.description_long,
+        #             'coordinates': {
+        #                 'lng': place.longitude,
+        #                 'lat': place.latitude,
+        #             },
+        #         },
+        #     },
+        # }
+
+        feature = {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.longitude, place.latitude],
+            },
+            'properties': {
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': '/static/places/moscow_legends.json',
+            },
+        }
+
+        data['features'].append(feature)
+
+    print(data)
+
+    return render(request, 'index.html', context={'data': data})
