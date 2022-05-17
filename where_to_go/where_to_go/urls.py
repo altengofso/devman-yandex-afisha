097b3c2a3f063825a819f70a84553b426b28e1ec
altengof_so <altengofso@gmail.com>
2022-05-17 13:48:02 +0300

    Add frontend

--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from .views import start_page
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', start_page),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
