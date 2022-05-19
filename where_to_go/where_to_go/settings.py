5adea1a47dc5b5f15f0de13862e34c8194a2ae5e
altengof_so <altengofso@gmail.com>
2022-05-19 15:36:43 +0300

    publish to pythonanywhere

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
